8083a77d4ee4f53d53486bdc09bc1242638bea1c
jeanrodriguesof <jeanrodriguesof367@gmail.com>
2023-10-30 07:46:43 -0200

    adicionado as aulas 04 até a 12

---
+++ aula12/ex12/ex12.js
@@ -0,0 +1,9 @@
+var hora = window.prompt("Digite a hora")
+
+if(hora < 12){
+    document.write("Bom dia!")
+} else if(hora <= 18){
+    document.write("Boa tarde")
+}else{
+    document.write("Bom dia")
+}
