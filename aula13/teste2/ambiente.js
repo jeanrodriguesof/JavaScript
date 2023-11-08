07cedcc3682411dce655af1ae62629cb1b409b0a
jeanrodriguesof <jeanrodriguesof367@gmail.com>
2023-11-08 07:52:20 -0200

    adicionado aula13 pt1

---
+++ aula13/teste2/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+while(c <= 6){
+    console.log(`passo ${c}`)
+
+    c++
+}
+
+
+
+/*
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+*/
